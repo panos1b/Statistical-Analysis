cf7cf0b6e46368aec69e7e8dcf403918ed0fc242
Panos Daskalopoulos <115027160+panos1b@users.noreply.github.com>
2024-04-14 13:27:33 +0300

    Added comments

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@
 class Program {
 	static void Main(string[] args)
 	{
+		// If there are no arguments we must produce an error
 		if (args.Length == 0 ) 
 		{
 			Console.WriteLine("You need to provide arguments. Type -h or --help for help");
 			return;
 		}
-
+		
+		// This will display the help for the repository
 		if ("-h".Equals(args[0]) || "--help".Equals(args[0]))
 		{
 			Console.WriteLine("This programm performs statistical analysis for a set of integers");
@@ -15,6 +17,7 @@ class Program {
 			return;
 		}
 
+		//We now start reading arguments. We will accept them in random possitions and mixed to make the programm more vercetile
 		List<int> ints = new List<int>();
 		bool min = false, max = false, modeFlag = false, meanFlag = false, medianFlag = false;
 		
@@ -24,6 +27,7 @@ class Program {
 			{
 				ints.Add(int.Parse(arg));
 			}
+			// If this fails we know its a flag argument
 			catch
 			{
 				switch(arg)
@@ -44,6 +48,7 @@ class Program {
 						medianFlag = true;
 						break;
 					default:
+						// This means they gave neither a flag neither an integer
 						Console.WriteLine("Invalid Argument. Type -h or --help for help");
 						return;
 				}
@@ -51,7 +56,7 @@ class Program {
 		}
 	
 		double median, mode = 0;
-
+		// Depending on which flags are active we do the required computations
 		if (min) 
 		{
 			Console.WriteLine($"The minimum is: {ints.Min()} ");
@@ -69,19 +74,20 @@ class Program {
 
 		if (medianFlag)
 		{
+			// To find the median we first sort
 			ints.Sort();
-			if (ints.Count % 2 == 0)
+			if (ints.Count % 2 == 0) // If there is an even amount of integers we take the two in the midle and divide them. By 2.0 to get a Double not an int
 			{
 				median = (ints[ints.Count / 2 - 1] + ints[ints.Count / 2]) / 2.0;
 			}
-			else 
+			else // There is a center element as they are odd
 			{
 				median = ints[ints.Count / 2];
 			}
 			Console.WriteLine($"The median is: {median}");
 		}
 
-		if (modeFlag) 
+		if (modeFlag) //Mode calculations are done by going through the repository from start to finnish and from finish to start and counting equallities
 		{
 			int maxCount = 0;
 
